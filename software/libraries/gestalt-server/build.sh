8c5deb185b6365ffdf6e960d7f09d84565250f2c
Daniel Lovell <lovell.daniel92@gmail.com>
2021-11-07 03:00:33 -0800

    gestalt-server added 'deploy' CLI arg to build.sh

--- software/libraries/gestalt-server/build.sh
+++ software/libraries/gestalt-server/build.sh
@@ -13,17 +13,23 @@ if [[ $@ == *clean* ]]; then
 fi
 
 rootdir=$(pwd)
+DEPLOY_DIR=$rootdir/../../apps/cor-ui/Assets/Plugins/
 BUILD_DIR=$rootdir/debug
 BUILD_TYPE=Debug
+WIN_BUILD_ARGS="//t:Rebuild //p:Configuration=Debug"
 
 if [[ $@ == *debug* ]]; then
     BUILD_DIR=$(pwd)/debug
     BUILD_TYPE=Debug
+    BUILD_SUFFIX=_d
+    WIN_BUILD_ARGS="//t:Rebuild //p:Configuration=Debug"
 fi
 
 if [[ $@ == *release* ]]; then
     BUILD_DIR=$(pwd)/release
     BUILD_TYPE=Release
+    BUILD_SUFFIX=
+    WIN_BUILD_ARGS="//t:Rebuild //p:Configuration=Release"
 fi
 
 echo "Building..."
@@ -34,10 +40,19 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Linux 
     cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
     make
+    BUILD_RESULT=$BUILD_DIR/gestalt_server$BUILD_SUFFIX.so
 elif [[ "$OSTYPE" == "msys" ]]; then
     # MinGW for Windows
     cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-    MSBuild.exe gestalt-server.sln
+    echo $WIN_BUILD_ARGS
+    echo $BUILD_TYPE
+    MSBuild.exe gestalt-server.sln $WIN_BUILD_ARGS
+    if [[ $@ == *debug* ]]; then
+        BUILD_RESULT=$BUILD_DIR/Debug/gestalt_server$BUILD_SUFFIX.dll
+    elif [[ $@ == *release* ]]; then
+        BUILD_RESULT=$BUILD_DIR/Release/gestalt_server$BUILD_SUFFIX.dll
+    fi
+    
 fi
 
 if [ $? -ne 0 ]; then
@@ -46,3 +61,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 cd $rootdir
+
+# Deploy the .dll/.so to cor-ui
+if [[ $@ == *deploy* ]]; then
+    cp -v $BUILD_RESULT $DEPLOY_DIR
+    if [ $? -ne 0 ]; then
+        echo "Deploy failed"
+        cd $rootdir
+        exit 1
+    fi
+fi
