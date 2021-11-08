ca9fdd9f9344c9b08a06c355833605c949730c63
Daniel Lovell <lovell.daniel92@gmail.com>
2021-11-07 19:02:26 -0800

    Further implementation of gestalt-solver

--- software/libraries/gestalt-solver/gestalt-solver.c
+++ software/libraries/gestalt-solver/gestalt-solver.c
@@ -1,10 +1,91 @@
 #include "gestalt-solver.h"
 
+/*
+ * Allocate a path_stream with path_length number of entries in all streams
+ */
+void alloc_path_stream(PathStream* path_stream, unsigned int path_length)
+{
+    path_stream->x_pos_stream = (float*)malloc(sizeof(float)*path_length);
+    path_stream->y_pos_stream = (float*)malloc(sizeof(float)*path_length);
+    path_stream->action_stream = (int*)malloc(sizeof(int)*path_length);
+    path_stream->exclusion_stream = (int*)malloc(sizeof(int)*path_length);
+    path_stream->path_length = path_length;
+}
+
+/*
+ * Allocate a StateVector with num_bots number of entries in bot streams and 
+ * num_cubes number of entries in the cube streams
+ */
+void alloc_state_vector(StateVector* state_vector, unsigned int num_bots, unsigned int num_cubes)
+{
+    state_vector->bot_x_pos_stream = (float*)malloc(sizeof(float)*num_bots);
+    state_vector->bot_y_pos_stream = (float*)malloc(sizeof(float)*num_bots);
+    state_vector->bot_ids = (int*)malloc(sizeof(int)*num_bots);
+    state_vector->num_bots = num_bots;
+    state_vector->cube_x_pos_stream = (float*)malloc(sizeof(float)*num_cubes);
+    state_vector->cube_y_pos_stream = (float*)malloc(sizeof(float)*num_cubes);
+    state_vector->cube_ids = (int*)malloc(sizeof(int)*num_cubes);
+    state_vector->num_cubes = num_cubes;
+}
+
+/*
+ * Free a path_stream
+ */
+void free_path_stream(PathStream* path_stream)
+{
+    free(path_stream->x_pos_stream);
+    free(path_stream->y_pos_stream);
+    free(path_stream->action_stream);
+    free(path_stream->exclusion_stream);
+}
+
+/*
+ * Free a StateVector
+ */
+void free_state_vector(StateVector* state_vector)
+{
+    free(state_vector->bot_x_pos_stream);
+    free(state_vector->bot_y_pos_stream);
+    free(state_vector->bot_ids);
+    free(state_vector->cube_x_pos_stream);
+    free(state_vector->cube_y_pos_stream);
+    free(state_vector->cube_ids);
+}
+
+/*
+ * Returns Euclidean distance between two 2D vectors
+ */
+float get_2d_dist(float x1, float y1, float x2, float y2)
+{
+    return sqrtf(powf(x2-x1, 2) + powf(y2-y1, 2));
+}
 
 /*
  * Solve the path streams and return the solution
+ *  - returns num_path_streams
+ *  - Populates path_stream_solution struct as OUTPUT
+ *  - Takes inital_state and final_state vectors as INPUT
  */
-PathStreamSolution* solve_pathstream(StateVector* initial_state, StateVector* final_state) 
+unsigned int solve_pathstream(PathStreamSolution* path_stream_solution, StateVector* initial_state, StateVector* final_state)
 {
+    // Naive solution - move each bot towards
+    PathStream** path_stream_vector = (PathStream**)malloc(sizeof(PathStream*) * initial_state->num_bots);
+    unsigned int path_length = 4;
+    for(unsigned int i = 0; i < initial_state->num_bots; i++) 
+    {
+        path_stream_vector[i] = (PathStream*)malloc(sizeof(PathStream));
+        alloc_path_stream(path_stream_vector[i], path_length);
+    }
+
+
+    PathStreamSolution* ps_solution = malloc(sizeof(struct PathStreamSolution));
+    for(unsigned int i = 0; i < initial_state->num_cubes; i++) 
+    {
+        for(unsigned int j = 0; j < initial_state->num_bots; j++)
+        {
+
+        }
+    }
+
     return NULL;
 }
--- software/libraries/gestalt-solver/gestalt-solver.h
+++ software/libraries/gestalt-solver/gestalt-solver.h
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 
 /* An individual path_stream, containing all path information for a single bot */
 typedef struct PathStream
 {
     float* x_pos_stream;
     float* y_pos_stream;
+    int* action_stream;
     int* exclusion_stream;
-    unsigned int* path_length;
+    unsigned int path_length;
     int bot_id;
 } PathStream;
 
 /* A complete path_stream solution, containing all path_streams for all bots in the environment */
 typedef struct PathStreamSolution
 {
-    PathStream* path_stream_vector;
-    unsigned int* num_path_streams;
+    PathStream** path_stream_vector;
+    unsigned int num_path_streams;
 } PathStreamSolution;
 
 typedef struct StateVector 
@@ -23,10 +27,46 @@ typedef struct StateVector
     float* bot_y_pos_stream;
     int* bot_ids;
     unsigned int num_bots;
+    float* cube_x_pos_stream;
+    float* cube_y_pos_stream;
+    int* cube_ids;
+    unsigned int num_cubes;
 } StateVector;
 
 
-PathStreamSolution* solve_pathstream(StateVector* initial_state, StateVector* final_state);
+/*
+ * Allocate a PathStream with path_length number of entries in all streams
+ */
+void alloc_path_stream(PathStream* path_stream, unsigned int path_length);
+
+/*
+ * Allocate a StateVector with num_bots number of entries in bot streams and 
+ * num_cubes number of entries in the cube streams
+ */
+void alloc_state_vector(StateVector* state_vector, unsigned int num_bots, unsigned int num_cubes);
+
+/*
+ * Free a PathStream
+ */
+void free_path_stream(PathStream* path_stream);
+
+/*
+ * Free a StateVector
+ */
+void free_state_vector(StateVector* state_vector);
+
+/*
+ * Returns Euclidean distance between two 2D vectors
+ */
+float get_2d_dist(float x1, float y1, float x2, float y2);
+
+/*
+ * Solve the path streams and return the solution
+ *  - returns num_path_streams
+ *  - Populates path_stream_solution struct as OUTPUT
+ *  - Takes inital_state and final_state vectors as INPUT
+ */
+unsigned int solve_pathstream(PathStreamSolution* path_stream_solution, StateVector* initial_state, StateVector* final_state);
 
 
 
