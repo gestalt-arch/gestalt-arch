71a0347f4a6213f8359af183fb94c22da140b811
Clare Lin <2cyl@berkeley.edu>
2021-11-12 22:54:04 -0800

    cube drag

---
+++ software/apps/cor-ui/Assets/Scripts/CubePositions.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubePositions : MonoBehaviour
+{
+    private Text cubeText;
+    private Vector3 cube1Pos;
+    private Vector3 cube2Pos;
+    private Vector3 cube3Pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeText = GameObject.Find("Main Camera/Canvas/Panel/Locations").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube1Pos = GameObject.Find("Cube 1").transform.position;
+        cube2Pos = GameObject.Find("Cube 2").transform.position;
+        cube3Pos = GameObject.Find("Cube 3").transform.position;
+        cubeText.text = "Cute Cube: " + 
+                        cube1Pos.ToString("F2") + "\nAngy Cube: " +
+                        cube2Pos.ToString("F2") + "\nPepe Cube: " +
+                        cube3Pos.ToString("F2");
+    }
+}
