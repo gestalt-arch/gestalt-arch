cb761837b79dd9750b93cae259defe793afb7c6f
Daniel Lovell <lovell.daniel92@gmail.com>
2021-11-16 16:49:39 -0800

    Added function definitions to gestalt-client

--- firmware/libraries/gestalt-client/gestalt-client.h
+++ firmware/libraries/gestalt-client/gestalt-client.h
@@ -1,7 +1,24 @@
 
 #include "kobukiSensorTypes.h"
 
+#define MAX_PATH 128
 
+typedef enum {
+    GESTALT_MOVE,
+    GESTALT_GRAB,
+    GESTALT_DROP
+} Gestalt_action_t;
+
+typedef struct {
+    float curr_x_pos;
+    float curr_y_pos;
+    float curr_rotation;
+} Gestalt_status_t;
+
+typedef struct {
+    float curr_x_goal;
+    float curr_y_goal;
+} Gestalt_goal_t;
 
 typedef struct {
     KobukiSensors_t* kobuki_sensors;
@@ -9,11 +26,18 @@ typedef struct {
 
 } Gestalt_sensor_data_t;
 
-// Receive sensor data 
+typedef struct {
 
-void gestalt_update_sensor_data(Gestalt_sensor_data_t* sensor_data);
+}
 
+void gestalt_update_pathstream
+
+// Receive sensor data
+void gestalt_update_sensor_data(const Gestalt_sensor_data_t* sensor_data);
 
 // Return action
+Gestalt_goal_t gestalt_get_current_goal();
+
+Gestalt_status_t gestalt_get_current_status();
 
 // Retur
