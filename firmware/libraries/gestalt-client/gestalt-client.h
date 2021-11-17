afb393357cbe0c59ba89a2a8467358fbc0bbda23
Daniel Lovell <lovell.daniel92@gmail.com>
2021-11-16 17:28:14 -0800

    gestalt-client added pathstream structs and functions

--- firmware/libraries/gestalt-client/gestalt-client.h
+++ firmware/libraries/gestalt-client/gestalt-client.h
@@ -1,7 +1,8 @@
 
 #include "kobukiSensorTypes.h"
 
-#define MAX_PATH 128
+#define MAX_SOLUTION_LENGTH 64
+#define MAX_BOTS 4
 
 typedef enum {
     GESTALT_MOVE,
@@ -18,6 +19,7 @@ typedef struct {
 typedef struct {
     float curr_x_goal;
     float curr_y_goal;
+    Gestalt_action_t curr_action_goal;
 } Gestalt_goal_t;
 
 typedef struct {
@@ -26,12 +28,27 @@ typedef struct {
 
 } Gestalt_sensor_data_t;
 
-void gestalt_update_pathstream
+typedef struct {
+    float x_pos_stream[MAX_SOLUTION_LENGTH];
+    float y_pos_stream[MAX_SOLUTION_LENGTH];
+    int action_stream[MAX_SOLUTION_LENGTH];
+    int exclusion_stream[MAX_SOLUTION_LENGTH];
+    unsigned int path_length;
+    int bot_id;
+} Gestalt_path_stream_t;
+
+typedef struct {
+    Gestalt_path_stream_t* path_stream_vector[MAX_BOTS];
+    unsigned int num_path_streams;
+} Gestalt_path_stream_sol_t;
+
+// Upload the pathstream solution
+void gestalt_update_pathstream_sol(Gestalt_path_stream_sol_t* path);
 
 // Receive sensor data
 void gestalt_update_sensor_data(const Gestalt_sensor_data_t* sensor_data);
 
-// Return action
+// Returns the current goal position and action
 Gestalt_goal_t gestalt_get_current_goal();
 
 Gestalt_status_t gestalt_get_current_status();
