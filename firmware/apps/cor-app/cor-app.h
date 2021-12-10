04f73a13ec964b153404860897c2925d162df8a6
daniellovell <lovell.daniel92@gmail.com>
2021-12-09 16:00:50 -0800

    Fix all build preventing errors

--- firmware/apps/cor-app/cor-app.h
+++ firmware/apps/cor-app/cor-app.h
@@ -1,12 +1,30 @@
-#include "gestalt-client.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
 
+#include "app_error.h"
 #include "nrf.h"
-#include "nrf_drv_clock.h"
-#include "nrf_uarte.h"
-#include "nrf_serial.h"
-#include "app_timer.h"
 #include "nrf_delay.h"
+#include "nrfx_gpiote.h"
+#include "nrf_gpio.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+#include "nrf_pwr_mgmt.h"
+#include "nrf_serial.h"
+#include "nrf_twi_mngr.h"
+
 #include "buckler.h"
+#include "display.h"
+
+#include "kobukiActuator.h"
+#include "kobukiSensorTypes.h"
+#include "kobukiSensorPoll.h"
+#include "kobukiUtilities.h"
+#include "lsm9ds1.h"
+
+#include "gestalt-client.h"
 
 typedef enum {
   STOP,
